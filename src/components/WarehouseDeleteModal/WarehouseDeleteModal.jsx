12a1efb5cb924c236f61742ba55db60dfb9e9ae3
Courtney S <courtneyscrimshaw@gmail.com>
2024-11-15 06:17:16 -0500

    removing react modal library to make the modal actually work

--- src/components/WarehouseDeleteModal/WarehouseDeleteModal.jsx
+++ src/components/WarehouseDeleteModal/WarehouseDeleteModal.jsx
@@ -1,7 +1,7 @@
 import "./WarehouseDeleteModal.scss";
-import Modal from "react-modal";
+// import Modal from "react-modal";
 
-Modal.setAppElement("#root");
+// Modal.setAppElement("#root");
 
 const WarehouseDeleteModal = ({
   isOpen,
@@ -23,18 +23,21 @@ const WarehouseDeleteModal = ({
   };
 
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      className="modalContent"
-      overlayClassName="modalOverlay"
-      contentLabel="Delete Warehouse"
-    >
+    // <Modal
+    //   isOpen={isOpen}
+    //   onRequestClose={onClose}
+    //   className="modalContent"
+    //   overlayClassName="modalOverlay"
+    //   contentLabel="Delete Warehouse"
+    // >
+    <div className="modal__container">
+        <div className="modal__content">
           <h1>Delete {warehouseName} warehouse?</h1>
           <h2>
             Please confirm you'd like to delete the {warehouseName} warehouse
             from the list of warehouses. You won't be able to undo this action.
           </h2>
+          </div>
 
         <div className="modalButtons">
           <button className="cancel" onClick={handleCancelClick}>
@@ -44,7 +47,9 @@ const WarehouseDeleteModal = ({
             Delete
           </button>
         </div>
-    </Modal>
+        </div>
+        
+   
   );
 };
 

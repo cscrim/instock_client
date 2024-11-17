8c2af8480c8e0b854a8cfcefa56cda6e9462f4bc
KayJayGoosay <kjguce@gmail.com>
2024-11-17 15:38:07 -0500

    update styling to table format for inventory list and inventory list on warehouse details page

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./InventoryList.scss"; // Updated styling
+import "./InventoryList.scss";
 import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
 import editIcon from "../../assets/Icons/edit-24px.svg";
 import chevronIcon from "../../assets/Icons/chevron_right-24px.svg";
 import sortIcon from "../../assets/Icons/sort-24px.svg";
 
-const InventoryList = ({ inventoryItems, onDelete, hideWarehouseColumn }) => {
-  // State to track if the view is mobile
+const InventoryList = ({ inventoryItems, hideWarehouseColumn, onDelete }) => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-  // Update state on window resize
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
@@ -22,94 +20,13 @@ const InventoryList = ({ inventoryItems, onDelete, hideWarehouseColumn }) => {
 
   return (
     <div className="inventory-list">
-      {/* Header row for tablet and desktop views */}
-      <div className="inventory-list__header-row">
-        <div className="header-row__title-box">
-          <div className="inventory-list__header-item">INVENTORY ITEM</div>
-          <img src={sortIcon} alt="sort icon" className="sort-icon" />
-        </div>
-        <div className="header-row__title-box">
-          <div className="inventory-list__header-item">CATEGORY</div>
-          <img src={sortIcon} alt="sort icon" className="sort-icon" />
-        </div>
-        <div className="header-row__title-box">
-          <div className="inventory-list__header-item">STATUS</div>
-          <img src={sortIcon} alt="sort icon" className="sort-icon" />
-        </div>
-        <div className="header-row__title-box">
-          <div className="inventory-list__header-item">QTY</div>
-          <img src={sortIcon} alt="sort icon" className="sort-icon" />
-        </div>
-        {!hideWarehouseColumn && (
-          <div className="header-row__title-box">
-            <div className="inventory-list__header-item">WAREHOUSE</div>
-            <img src={sortIcon} alt="sort icon" className="sort-icon" />
-          </div>
-        )}
-        <div className="inventory-list__header-item">ACTIONS</div>
-      </div>
-
-      {/* Rows for inventory items */}
-      {inventoryItems.map((item) => (
-        <div key={item.id} className="inventory-list__row">
-          {/* Conditional layout for mobile or larger screens */}
-          {isMobile ? (
-            <div className="inventory-list__content-box">
-              <div className="list-column">
-                <div className="inventory-list__item">
-                  <div className="inventory-list__header-item">
-                    INVENTORY ITEM
-                  </div>
-                  <div className="item-box">
-                    <Link
-                      to={`/inventory/details/${item.id}`}
-                      className="inventory-list__name-link"
-                    >
-                      {item.item_name}
-                    </Link>
-                    <img
-                      src={chevronIcon}
-                      className="item-chevron"
-                      alt="chevron icon pointing right"
-                    />
-                  </div>
-                </div>
-                <div className="inventory-list__item">
-                  <div className="inventory-list__header-item">CATEGORY</div>
-                  <div>{item.category}</div>
-                </div>
-              </div>
-              <div className="list-column">
-                <div className="inventory-list__item">
-                  <div className="inventory-list__header-item">STATUS</div>
-                  <div
-                    className={
-                      item.status === "Out of Stock"
-                        ? "item__outstock"
-                        : "item__instock"
-                    }
-                  >
-                    {item.status}
-                  </div>
-                </div>
-                <div className="inventory-list__item">
-                  <div className="inventory-list__header-item inventory-list__header-item--qty">
-                    QTY
-                  </div>
-                  <div>{item.quantity}</div>
-                </div>
-                {!hideWarehouseColumn && (
-                  <div className="inventory-list__item">
-                    <div className="inventory-list__header-item">WAREHOUSE</div>
-                    <div>{item.warehouse_name}</div>
-                  </div>
-                )}
-              </div>
-            </div>
-          ) : (
-            <>
-              {/* <div className="list-column"> */}
+      {isMobile ? (
+        // Mobile view
+        inventoryItems.map((item) => (
+          <div key={item.id} className="inventory-list__content-box">
+            <div className="list-column">
               <div className="inventory-list__item">
+                <div className="inventory-list__header-item">ITEM NAME</div>
                 <div className="item-box">
                   <Link
                     to={`/inventory/details/${item.id}`}
@@ -124,51 +41,147 @@ const InventoryList = ({ inventoryItems, onDelete, hideWarehouseColumn }) => {
                   />
                 </div>
               </div>
-              <div className="inventory-list__item inventory-list__item--padding">
-                {item.category}
+              <div className="inventory-list__item">
+                <div className="inventory-list__header-item">CATEGORY</div>
+                <div>{item.category}</div>
               </div>
-              {/* </div> */}
-              {/* <div className="list-column"> */}
-              <div
-                className="inventory-list__item inventory-list__item--padding
-              "
-              >
-                <div
-                  className={
-                    item.status === "Out of Stock"
-                      ? "item__outstock"
-                      : "item__instock"
-                  }
-                >
+            </div>
+            <div className="list-column">
+              <div className="inventory-list__item">
+                <div className="inventory-list__header-item">QUANTITY</div>
+                <div>{item.quantity}</div>
+              </div>
+              <div className="inventory-list__item">
+                <div className="inventory-list__header-item">STATUS</div>
+                <div className={`item__${item.status.toLowerCase()}`}>
                   {item.status}
                 </div>
               </div>
-              <div className="inventory-list__item inventory-list__item--padding">
-                {item.quantity}
-              </div>
               {!hideWarehouseColumn && (
-                <div className="inventory-list__item inventory-list__item--padding">
-                  {item.warehouse_name}
+                <div className="inventory-list__item">
+                  <div className="inventory-list__header-item">WAREHOUSE</div>
+                  <div>{item.warehouseName}</div>
                 </div>
               )}
-            </>
-          )}
-          <div className="inventory-list__item inventory-list__item--actions">
-            <button
-              className="inventory-list__button"
-              onClick={() => onDelete(item.id, item.item_name)}
-            >
-              <img src={deleteIcon} alt="delete trashcan icon" />
-            </button>
-            <Link
-              to={`/inventory/edit/${item.id}`}
-              className="inventory-list__button"
-            >
-              <img src={editIcon} alt="edit pencil icon" />
-            </Link>
+              <div className="inventory-list__item">
+                <div className="inventory-list__header-item">ACTIONS</div>
+                <button
+                  className="inventory-list__button"
+                  onClick={() => onDelete(item.id, item.name)}
+                >
+                  <img
+                    src={deleteIcon}
+                    alt="delete trashcan icon"
+                    className="button__delete-icon"
+                  />
+                </button>
+                <Link
+                  to={`/inventory/edit/${item.id}`}
+                  className="inventory-list__button"
+                >
+                  <img
+                    src={editIcon}
+                    alt="edit pencil icon"
+                    className="button__edit-icon"
+                  />
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        // Tablet/Desktop view as a table
+        <table className="inventory-list__table">
+          <thead>
+            <tr>
+              <th>
+                <div className="header-title">
+                  ITEM NAME
+                  <img src={sortIcon} alt="sort icon" className="sort-icon" />
+                </div>
+              </th>
+              <th>
+                <div className="header-title">
+                  CATEGORY
+                  <img src={sortIcon} alt="sort icon" className="sort-icon" />
+                </div>
+              </th>
+              <th>
+                <div className="header-title">
+                  QUANTITY
+                  <img src={sortIcon} alt="sort icon" className="sort-icon" />
+                </div>
+              </th>
+              <th>
+                <div className="header-title">
+                  STATUS
+                  <img src={sortIcon} alt="sort icon" className="sort-icon" />
+                </div>
+              </th>
+              {!hideWarehouseColumn && (
+                <th>
+                  <div className="header-title">
+                    WAREHOUSE
+                    <img src={sortIcon} alt="sort icon" className="sort-icon" />
+                  </div>
+                </th>
+              )}
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {inventoryItems.map((item) => (
+              <tr key={item.id}>
+                <td>
+                  <div className="item-box">
+                    <Link
+                      to={`/inventory/details/${item.id}`}
+                      className="inventory-list__name-link"
+                    >
+                      {item.item_name}
+                    </Link>
+                    <img
+                      src={chevronIcon}
+                      className="item-chevron"
+                      alt="chevron icon pointing right"
+                    />
+                  </div>
+                </td>
+                <td>{item.category}</td>
+                <td>{item.quantity}</td>
+                <td>
+                  <span className={`item__${item.status.toLowerCase()}`}>
+                    {item.status}
+                  </span>
+                </td>
+                {!hideWarehouseColumn && <td>{item.warehouse_name}</td>}
+                <td>
+                  <button
+                    className="inventory-list__button"
+                    onClick={() => onDelete(item.id, item.name)}
+                  >
+                    <img
+                      src={deleteIcon}
+                      alt="delete trashcan icon"
+                      className="button__delete-icon"
+                    />
+                  </button>
+                  <Link
+                    to={`/inventory/edit/${item.id}`}
+                    className="inventory-list__button"
+                  >
+                    <img
+                      src={editIcon}
+                      alt="edit pencil icon"
+                      className="button__edit-icon"
+                    />
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
